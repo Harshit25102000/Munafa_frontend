db4738dafbd56496ac4045e62504987854bca80b
Harshit Singh <harshit25102000@gmail.com>
2024-07-30 17:25:06 +0530

    latest

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,101 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './home.css';
 import { BACKEND_URL } from "../../config";
 import NavbarComponent from '../../components/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import ApexCharts from 'apexcharts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useNavigate } from "react-router-dom"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Chart from 'chart.js/auto';
+import Alerts from '../../components/alert';
 function Home() {
-
+    const [loading,setLoading]=useState(false)
+  const [variant,setVariant]=useState("");
+  const [alertMessage,setAlertMessage]=useState("")
+  const [isalert,setIsalert]=useState(false)
     const [email, setEmail] = useState("")
     const [error, setErrorMessage] = useState("")
     const [classdata, setClassData] = useState([])
+    const [isAdd,setIsAdd]=useState(false)
     const navigate = useNavigate();
+    const [date,setDate]=useState([])
+    const [close,setClose]=useState([])
+    const chartRef = useRef(null);
+    let myChart = null;
+    const handleAdd = () => {
+        setIsAdd(true)
+    }
+
+    const fetchChartData = () => {
+        fetch(BACKEND_URL + '/get_fetch_data', {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                withCredentials: true
+            },
+
+
+        })
+
+            .then(async (response) => {
+                const data = await response.json();
+                if (data.success && data.data && data.data.status === 'SUCCESS') {
+
+                    setClose(data.data.close);
+                    setDate(data.data.date);
+
+
+                } else {
+                    setErrorMessage(data.message || 'Failed. Please try again.');
+                    setVariant("danger")
+                    setAlertMessage(error)
+                    setLoading(false)
+                  
+                    setIsalert(true)
+                  }
+                })
+                .catch((error) => {
+                  setVariant("danger")
+                  setAlertMessage(error)
+                  setLoading(false)
+               
+                  setIsalert(true)
+        
+                });
+
+    }
+
+    useEffect(() => {
+        if (myChart) {
+            myChart.destroy();
+        }
+        const ctx = chartRef.current.getContext('2d');
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [
+                    {
+                        label: 'Price Overview',
+                        data: close,
+                        borderColor: 'blue',
+                        backgroundColor: 'rgba(0, 0, 255, 0.2)',
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+            },
+
+        });
+
+    }, []);
     useEffect(() => {
 
 
@@ -56,6 +136,9 @@ function Home() {
     return (
         <div>
             <NavbarComponent />
+            {isalert && (
+        <Alerts variant={variant} message={alertMessage} isalert={true} />
+       )}
             <header id="header" class="d-flex align-items-center" style={{ backgroundColor: "#f2f2f2" }}>
 
 
@@ -107,7 +190,21 @@ function Home() {
                 <Row>
                     <Col>
                         <div>
-                            <h5 style={{ margin: "47px 0px 0px 50px", cursor: "pointer" }}>Watchlist</h5>
+                            <h5 style={{ margin: "47px 0px 0px 50px", cursor: "pointer" }}>Watchlist<button style={{"marginLeft":"10px","background":"None"}} onClick={handleAdd}>+</button></h5>
+                            {isAdd && (
+         <form class="form-inline">
+         <div class="form-group mb-2">
+             <label for="staticEmail2" class="sr-only">Email</label>
+             <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Add to Watchlist"/>
+         </div>
+         <div class="form-group mx-sm-3 mb-2">
+             <label for="inputPassword2" class="sr-only">Password</label>
+             <input type="password" class="form-control" id="inputPassword2" placeholder="Symbol"/>
+         </div>
+         <button type="submit" class="btn btn-primary mb-2">Add</button>
+     </form>
+       )}
+                           
                         </div>
                         <div style={{ overflowY: "scroll", height: "530px" }}>
                             <Card style={{ marginTop: "30px", width: "85%" }}>
@@ -115,16 +212,21 @@ function Home() {
                                 <Card.Body>
                                     <Row>
                                         <Col>
-                                            <Card.Title style={{ fontWeight: "bold" }}>TATA</Card.Title>
+                                            <Button onClick={fetchChartData}><Card.Title style={{ fontWeight: "bold" }}>TATA</Card.Title></Button>
                                             <Card.Text>
-                                                Rs 312.78
+                                                BID - 23.5
+                                            </Card.Text>
+                                            <Card.Text>
+                                                ASK - 23.5
                                             </Card.Text>
                                         </Col>
 
                                         <Col>
-
-                                            <Button variant="primary" style={{ marginTop: "10px", backgroundColor: "green",marginRight:"2px" }} onClick={() => navigate("/attendance")}>BUY</Button>
-                                            <Button variant="primary" style={{ marginTop: "10px", backgroundColor: "red",marginLeft:"2px" }} onClick={() => navigate("/attendance")}>SELL</Button>
+                                            <Card.Text>
+                                                Price - 23.5
+                                            </Card.Text>
+                                            <Button variant="primary" style={{ marginTop: "10px", backgroundColor: "green", marginRight: "2px" }} onClick={() => navigate("/attendance")}>BUY</Button>
+                                            <Button variant="primary" style={{ marginTop: "10px", backgroundColor: "red", marginLeft: "2px" }} onClick={() => navigate("/attendance")}>SELL</Button>
                                         </Col>
                                     </Row>
                                 </Card.Body>
@@ -147,14 +249,10 @@ function Home() {
                     <Col>
 
                         <div style={{ marginTop: "5%" }}>
-
-                            <h3 style={{ marginBottom: "5%" }}>Add something to your watchlist</h3>
-                            <div class="input-group mb-3" style={{ width: "50%" }}>
-                                <div class="input-group-prepend">
-                                    <button class="btn btn-outline-secondary" type="button">Add</button>
-                                </div>
-                                <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" />
+                            <div className="chart-container">
+                                <canvas ref={chartRef} style={{ height: "35vh" }} />
                             </div>
+
                         </div>
                     </Col>
                 </Row>
